aba84d79e1260c43360b510f970ae56ea7b57070
guoyingxin7 <guoyingxin7@jd.com>
2022-07-17 11:23:46 +0800

    style: 🎨 run lint script's changes

--- src/views/link/github/index.tsx
+++ src/views/link/github/index.tsx
@@ -1,17 +1,17 @@
 import "./index.less";
 
 const Github = () => {
-  return (
-    <div className="content-box">
-      <span className="text">
-        Github 仓库：
-        <a href="#" target="_blank" rel="noreferrer">
-          #
-        </a>{" "}
-        🍒🍉🍊
-      </span>
-    </div>
-  );
+	return (
+		<div className="content-box">
+			<span className="text">
+				Github 仓库：
+				<a href="#" target="_blank" rel="noreferrer">
+					#
+				</a>{" "}
+				🍒🍉🍊
+			</span>
+		</div>
+	);
 };
 
 export default Github;
