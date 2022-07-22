b8752f32ffa5f568df76769c09b2210113d82275
guoyingxin7 <guoyingxin7@jd.com>
2022-07-22 23:12:03 +0800

    refactor(link): ♻️ remove custom link types

--- src/routers/modules/link.tsx
+++ src/routers/modules/link.tsx
@@ -12,42 +12,6 @@ const linkRouter: Array<RouteObject> = [
 		},
 		children: [
 			{
-				path: "/link/gitee",
-				element: lazyLoad(React.lazy(() => import("@/views/link/gitee/index"))),
-				meta: {
-					requiresAuth: true,
-					title: "Gitee 仓库",
-					key: "gitee"
-				}
-			},
-			{
-				path: "/link/github",
-				element: lazyLoad(React.lazy(() => import("@/views/link/github/index"))),
-				meta: {
-					requiresAuth: true,
-					title: "GitHub 仓库",
-					key: "github"
-				}
-			},
-			{
-				path: "/link/juejin",
-				element: lazyLoad(React.lazy(() => import("@/views/link/juejin/index"))),
-				meta: {
-					requiresAuth: true,
-					title: "掘金文档",
-					key: "juejin"
-				}
-			},
-			{
-				path: "/link/myBlog",
-				element: lazyLoad(React.lazy(() => import("@/views/link/myBlog/index"))),
-				meta: {
-					requiresAuth: true,
-					title: "个人博客",
-					key: "myBlog"
-				}
-			},
-			{
 				path: "/link/default",
 				element: lazyLoad(React.lazy(() => import("@/views/link/default/index"))),
 				meta: {
