01bfdcea6b96e3ce1a2884c68b7ff71344bea631
guoyingxin7 <guoyingxin7@jd.com>
2022-07-23 18:34:30 +0800

    :art: chore: util.tsx updated;

--- src/layouts/util.tsx
+++ src/layouts/util.tsx
@@ -3,8 +3,16 @@ import * as Icons from "@ant-design/icons";
 
 // 动态渲染 Icon 图标
 const customIcons: { [key: string]: any } = Icons;
-export const addIcon = (name: string) => {
+
+// type: Outlined / Filled / TwoTone
+export const ICON_TYPE = {
+	Outlined: "Outlined",
+	Filled: "Filled",
+	TwoTone: "TwoTone"
+};
+
+export const addIcon = (name: string, type: string = ICON_TYPE.Outlined) => {
 	const _name = name.replace(/^\w/, c => c.toUpperCase());
-	const icon = customIcons[_name] || customIcons[`${_name}Outlined`] || customIcons["QuestionOutlined"];
+	const icon = customIcons[_name] || customIcons[`${_name}${type}`] || customIcons[`Question${type}`];
 	return React.createElement(icon);
 };
